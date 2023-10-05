f2a2ace8b866de4e46f700d17b9cdc57a8164334
zhangqian <244830512@qq.com>
2023-10-05 21:38:39 +0800

    选择版本号

--- changeDebianSource/changeDebianSource.sh
+++ changeDebianSource/changeDebianSource.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
 
+function changeDebianSource() {
+	# 显示菜单
+	OPTION=$(whiptail --title " 更改源 " --menu "
+# 请选择相应的版本：" 25 60 15 \
+		"b" "pve7" \
+		"c" "pve8" \
+		3>&1 1>&2 2>&3)
+	exitstatus=$?
+	if [ $exitstatus = 0 ]; then
+		case "$OPTION" in
+		b | B)
+			echo "选择了pve7"
+			;;
+		c | C)
+			echo "选择了pve8"
+			;;
+		esac
+	fi
+}
--- install.sh
+++ install.sh
@@ -10,3 +10,17 @@ Github: $GITHUB_URL
 	"b" "更换源" \
 	"x" "关于脚本" \
 	3>&1 1>&2 2>&3)
+
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+	case "$OPTION" in
+	b | B)
+		echo "选择了更换源"
+		source $current_path/changeDebianSource/changeDebianSource.sh
+		changeDebianSource
+		;;
+	x | X)
+		echo "关于脚本"
+		;;
+	esac
+fi
